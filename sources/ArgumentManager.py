9e08e04f39567b3d885fd67e5155f75e0d28acde
Corentin COUTRET-ROZET <corentin.rozet@epitech.eu>
2020-11-09 18:37:35 +0100

    [ADD] ArgumentManager

--- sources/ArgumentManager.py
+++ sources/ArgumentManager.py
@@ -8,6 +8,8 @@
 #                                          #
 ############################################
 
+from os import stat
+from os.path import isfile
 
 class ArgumentManager():
 
@@ -17,6 +19,15 @@ class ArgumentManager():
         Check for input arguments validity.
         """
 
+        if len(argv) != 2:
+            print("ERROR: wrong number of arguments.")
+            return 84
+        if isfile(argv[1]) is False:
+            print(f"ERROR: {argv[1]}: no such file.")
+            return 84
+        if stat(argv[1]).st_size == 0:
+            print(f"ERROR: {argv[1]}: empty file.")
+            return 84
         return 0
 
     def needHelp(self, argv) -> bool:
