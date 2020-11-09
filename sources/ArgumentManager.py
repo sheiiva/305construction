d447ff292ae2e14bb880eb8ba9ed7b94ef4db770
Corentin COUTRET-ROZET <corentin.rozet@epitech.eu>
2020-11-09 18:17:09 +0100

    [ADD] ArgumentManager

--- sources/ArgumentManager.py
+++ sources/ArgumentManager.py
@@ -4,11 +4,9 @@
 #                                          #
 #           COUTRET-ROZET Corentin         #
 #                                          #
-#            Project : 304pacman           #
+#          Project : 305construction       #
 #                                          #
 ############################################
-
-import os
 
 
 class ArgumentManager():
@@ -19,30 +17,6 @@ class ArgumentManager():
         Check for input arguments validity.
         """
 
-        def isValidFile(filename: str) -> bool:
-
-            """Check if filename is an existing file."""
-
-            try:
-                f = open(filename)
-            except IOError:
-                print("{} is not a valid file.".format(filename))
-                return False
-            else:
-                f.close()
-                if os.stat(filename).st_size == 0:
-                    print("{} is empty.".format(filename))
-                    return False
-                return True
-
-        if len(argv) != 4:
-            print("Wrong number of arguments.")
-            return 84
-        if isValidFile(argv[1]) is False:
-            return 84
-        if argv[2] == argv[3]:
-            print("Please enter different values for `c1` and `c2`.")
-            return 84
         return 0
 
     def needHelp(self, argv) -> bool:
