22d4602550edc4d0c53188eeade69e1272f3b843
Corentin COUTRET-ROZET <corentin.rozet@epitech.eu>
2020-11-09 19:04:38 +0100

    [ADD] Task

--- sources/utils.py
+++ sources/utils.py
@@ -4,61 +4,24 @@
 #                                          #
 #           COUTRET-ROZET Corentin         #
 #                                          #
-#            Project : 304pacman           #
+#          Project : 305construction       #
 #                                          #
 ############################################
 
-EMPTY_SQUARE    = '0'
-WALL            = '1'
-GHOST           = 'F'
-PACMAN          = 'P'
 
-X = 0
-Y = 1
+def isInt(item) -> bool:
 
+    """Define if item is an integer or not.
 
-def UP(x: int, y: int) -> tuple:
-    return (x, y-1)
+    Args:
+        item (?): Variable to check.
 
-
-def RIGHT(x: int, y: int) -> tuple:
-    return (x+1, y)
-
-
-def DOWN(x: int, y: int) -> tuple:
-    return (x, y+1)
-
-
-def LEFT(x: int, y: int) -> tuple:
-    return (x-1, y)
-
-
-DIR = {"UP": UP, "RIGHT": RIGHT, "DOWN": DOWN, "LEFT": LEFT}
-
-
-def GETUP(list: list, x: int, y: int):
-    return list[y-1][x]
-
-
-def GETRIGHT(list: list, x: int, y: int):
-    return list[y][x+1]
-
-
-def GETDOWN(list: list, x: int, y: int):
-    return list[y+1][x]
-
-
-def GETLEFT(list: list, x: int, y: int):
-    return list[y][x-1]
-
-
-GETDIR = {"UP": GETUP, "RIGHT": GETRIGHT, "DOWN": GETDOWN, "LEFT": GETLEFT}
-
-
-def isDigit(elem) -> bool:
+    Returns:
+        bool: True if item is an integer, False otherwise.
+    """
 
     try:
-        int(elem)
+        int(item)
     except ValueError:
         return False
     else:
